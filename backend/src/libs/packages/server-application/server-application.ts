657d07b375d199dd833be3982456ebc25aebcb1c
jaroslawtt <yaroslavlogan61907@gmail.com>
2024-04-19 20:20:16 +0300

    Initialized project

--- backend/src/libs/packages/server-application/server-application.ts
+++ backend/src/libs/packages/server-application/server-application.ts
@@ -4,10 +4,7 @@ import { logger } from '~/libs/packages/logger/logger.js';
 import { ServerApp } from './server-app.js';
 import { ServerAppApi } from './server-app-api.js';
 
-const apiV1 = new ServerAppApi(
-  'v1',
-  config,
-);
+const apiV1 = new ServerAppApi('v1', config);
 const serverApp = new ServerApp({
   config,
   logger,
